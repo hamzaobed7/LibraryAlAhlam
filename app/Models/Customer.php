df1798cdc68d6dd2fbfd22e73d0f010890ee4458
hamzaobed7 <hamazobed@gmail.com>
2026-06-16 01:55:04 +0300

    add Prunable.

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,12 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Prunable;
+use Illuminate\Database\Eloquent\Builder;
 class Customer extends Model
 {
+    use Prunable;
     protected $table='customers';
     protected $fillable = ['name','gender','DOB','cover','phone','lang','user_id'];
-      public $timestamps = false;
+    
     public function user():BelongsTo{
         return $this->belongsTo(User::class);
     }
+
+    public function prunable(): Builder
+{
+    return static::whereHas('user', function ($q) {
+        $q->whereNull('email_verified_at');
+    })->where('created_at', '<', now()->subDay());
+}
 }
